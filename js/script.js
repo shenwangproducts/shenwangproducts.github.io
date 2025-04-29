f1b6be3a41cf5855746b2649e772e7a0cc899c0c
shenwangproducts <zeegodstudio04@gmail.com>
2025-04-30 03:23:35 +0800

    Update script.js
    Signed-off-by: shenwangproducts <zeegodstudio04@gmail.com>

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ function changePage(page) {
   message.onend = function () {
     switch (page) {
       case 'promo':
-        window.location.href = 'promo_display.html';
+        window.location.href = 'l';
         break;
       case 'news':
         window.location.href = 'news.html';
